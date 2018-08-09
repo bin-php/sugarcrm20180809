58541d91f9c046c128892335b15ebfa815cf13f7
bin-php <34760595+bin-php@users.noreply.github.com>
2018-08-09 08:18:35 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 通过接口插入数据的配置
+ */
+//sugarcrm接口的URL
+define('URL', 'http://gcdev.fulengen.cn:8080/sugarcrm/service/v4/rest.php');
+//sugarcrm接口的用户名
+define('USER', 'admin');
+//sugarcrm接口的密码
+define('PASS', '4Cd5snLbb');
+
+/**
+ * 直接连接数据库进行查询的配置
+ */
+//服务器地址
+define('HOST', 'localhost');
+//数据库用户名
+define('USERNAME', 'sugar');
+//数据库密码
+define('PASSWORD', 'qazwsx');
+//数据库名
+define('DB', 'sugarcrm');
